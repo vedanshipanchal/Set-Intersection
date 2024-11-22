d5f9dfdd7e549060137d34d7360233b774029ab5
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-22 16:36:44 +0000

    Set Intersection

---
+++ main.py
@@ -0,0 +1,10 @@
+Set1 = {"green", "blue"} 
+Set2 = {"blue", "yellow"}
+
+#intersection - taling common from Set1 and Set2
+answer = Set1.intersection(Set2)
+print("Answer is:",answer)
+
+#union - combining Set 1 and Set 2
+answer2 = Set1.union(Set2)
+print("Answer is",answer)
